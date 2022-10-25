303a868414dd59bb34c8c52abb634ea13bb9768c
Davi Reis <reis.davi11010@gmail.com>
2022-10-25 10:40:39 -0300

    :hammer: refactor(setup.sh): organized code

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,31 +1,33 @@
+# --> configure terminal
 function installFish {
   sudo apt-add-repository ppa:fish-shell/release-3 -y
   sudo apt update -y
   sudo apt install fish -y
   chsh -s $(which fish)
-
   fish
 }
 
 function installStarship {
   curl -sS 'https://starship.rs/install.sh' | sh
-  echo "starship init fish | source" >> ''~/.config/fish/config.fish''
+  echo "starship init fish | source" >> '~/.config/fish/config.fish'
+  source '~/.config/fish/config.fish'
+}
 
-  source ''~/.config/fish/config.fish''
+# --> install essential
+function installEssential {
+  sudo apt update -y
+  sudo apt install -y build-essential make libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev wget curl llvm git git-flow
+  sudo apt upgrade -y
 }
 
 function installASDF {
   git clone 'https://github.com/asdf-vm/asdf.git' '~/.asdf' --branch v0.10.2
-  echo "source ~/.asdf/asdf.fish" >> ''~/.config/fish/config.fish''
+  echo "source ~/.asdf/asdf.fish" >> '~/.config/fish/config.fish'
   mkdir -p '~/.config/fish/completions'; and ln -s '~/.asdf/completions/asdf.fish' '~/.config/fish/completions'
-
-  source ''~/.config/fish/config.fish''
+  source '~/.config/fish/config.fish'
 }
 
-function installGitFlow {
-  sudo apt install git-flow -y
-}
-
+# --> install languages
 function installRust {
   asdf plugin add rust
   asdf install rust latest
@@ -33,24 +35,10 @@ function installRust {
   fish_add_path '~/.asdf/installs/rust/1.64.0/bin'
 }
 
-function installCliTools {
-  sudo apt install build-essential -y
-  cargo install bat exa tokei hyperfine ytop
-}
-
-function createCliAliases {
-  echo 'alias cat="bat"' >> '~/.config/fish/config.fish'
-  echo 'alias ls="exa --icons"' >> '~/.config/fish/config.fish'
-  echo 'alias top="ytop"' >> '~/.config/fish/config.fish'
-
-  source '~/.config/fish/config.fish'
-}
-
-function installPython {
-  sudo apt-get install make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
-  asdf plugin add python
-  asdf install python latest
-  asdf global python latest
+function installRuby {
+  asdf plugin add ruby
+  asdf install ruby latest
+  asdf global ruby latest
 }
 
 function installNode {
@@ -59,16 +47,36 @@ function installNode {
   asdf global nodejs latest
 }
 
+function installPython {
+  asdf plugin add python
+  asdf install python latest
+  asdf global python latest
+}
+
+# --> install rust-alternatives
+function installRustAlternatives {
+  cargo install bat exa tokei hyperfine ytop
+}
+
+function createRustAlternativeAliases {
+  echo 'alias cat="bat"' >> '~/.config/fish/config.fish'
+  echo 'alias ls="exa --icons"' >> '~/.config/fish/config.fish'
+  echo 'alias top="ytop"' >> '~/.config/fish/config.fish'
+  source '~/.config/fish/config.fish'
+}
+
+# --> install lunarvim
 function installNeoVim {
   sudo add-apt-repository ppa:neovim-ppa/unstable
-  sudo apt-get update
-  sudo apt-get install neovim -y
+  sudo apt update
+  sudo apt install neovim@0.8 -y
 }
 
 function installLunarVim {
   curl -s 'https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh' | bash
 }
 
+# --> get dotfiles
 function getDotFiles {
   git clone 'https://github.com/dreisss/.files.git' ~/.files
 }
@@ -80,6 +88,5 @@ function createLinks {
 function updatePackages {
   sudo apt update -y
   sudo apt upgrade -y
-  sudo apt install build-essential -y
   sudo apt autoremove -y
 }
