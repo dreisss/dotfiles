348e9622e412ee08cae669edcf126db3d5689a47
Davi Reis <reis.davi11010@gmail.com>
2023-05-20 13:33:02 -0300

    :zap: chore: few updates

--- scripts/pkgs.sh
+++ scripts/pkgs.sh
@@ -8,7 +8,7 @@ sudo pacman -S cbqn-git sqlite mysql postgresql --noconfirm
 echo "$(rtx ls | sed -E -e 's/⏵ *| *\([a-z ~\/.-]*\)//g')" > ~/.tool-versions
 
 # install utils cli tools
-yay -S vhs gum glow slides --noconfirm
+yay -S vhs gum glow slides nodejs-live-server httpie --noconfirm
 go install github.com/maaslalani/nap@main
 go install github.com/maaslalani/draw@main
 go install github.com/maaslalani/crow@master
@@ -17,10 +17,9 @@ go install github.com/maaslalani/crow@master
 git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 
 # install rust cli-alternatives
-cargo install bat exa tokei hyperfine ytop zoxide xcp rm-improved ripgrep fd-find grex git-delta xh dprint pier hx swc
+cargo install bat exa tokei hyperfine ytop zoxide xcp rm-improved ripgrep fd-find grex git-delta pier hx swc
 
 # install lsps
-yay -S {bash,typescript,yaml,svelte,haskell}-language-server vscode-{css,html,json}-languageserver {marksman,crystalline}-bin clang rust-analyzer taplo-cli gopls --noconfirm
-pip install -U 'python-lsp-server[all]'
+yay -S {bash,typescript,yaml,svelte,haskell}-language-server vscode-langservers-extracted {marksman,crystalline}-bin clang rust-analyzer taplo-cli gopls --noconfirm
 gem install solargraph
-pnpm add -g @astrojs/language-server
+pnpm i -g pyright @astrojs/language-server @prisma/language-server eslint
