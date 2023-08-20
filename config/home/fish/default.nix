80f41295ff2214393bc77bee86070a9d21a45eb8
Davi Reis <dreisss026@gmail.com>
2023-08-20 14:10:36 -0300

    :zap: chore(fish): configured exa to print file git status

--- config/home/fish/default.nix
+++ config/home/fish/default.nix
@@ -8,9 +8,9 @@
       cd = "z";
       cp = "xcp";
       rm = "rip";
-      l = "exa --icons -s type -l";
+      l = "exa --icons -s type --git -l";
       ls = "exa --icons -s type";
-      la = "exa --icons -s type -la ";
+      la = "exa --icons -s type --git -la ";
       lt = "exa -s type --git-ignore -T";
       lta = "exa -s type --git-ignore -Ta --ignore-glob .git";
       cat = "bat";
