bf94fd14704e5b23353b123cfa356f694f04ffe5
Davi Reis <dreisss.dev@gmail.com>
2024-02-07 23:25:59 -0300

    feat(scripts): fix prisma env variables using script

--- config/home/default.nix
+++ config/home/default.nix
@@ -16,12 +16,6 @@
           EDITOR = "hx";
           BROWSER = "firefox";
           TERMINAL = "alacritty";
-
-          PRISMA_MIGRATION_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/migration-engine";
-          PRISMA_QUERY_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/query-engine";
-          PRISMA_INTROSPECTION_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/introspection-engine";
-          PRISMA_QUERY_ENGINE_LIBRARY = "${pkgs.prisma-engines}/lib/libquery_engine.node";
-          PRISMA_FMT_BINARY = "${pkgs.prisma-engines}/bin/prisma-fmt";
         };
       };
 
